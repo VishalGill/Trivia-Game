a2c94f289c71c28904e2889d8b2a74a5da87edda
VishalGill <vgill96@ymail.com>
2022-09-09 16:37:37 -0400

    redoing JS w/ psuedoscode done

--- trivia-game/p1.js
+++ trivia-game/p1.js
@@ -1,7 +1,28 @@
-let ul = document.getElementById('ul');
-let nextButton = document.getElementById('btnNext');
-let quizbox = document.getElementById('questionBox');
-let answer1 = document.getElementById('answer1');
-let answer2 = document.getElementById('answer2');
-let answer3 = document.getElementById('answer3');
-let answer4 = document.getElementById('answer4');
+//User Stories
+// AAU I want multiple choice answers
+// AAU I want to be able to write my name as the player name
+// AAU I want to be able to see my score
+// AAU I want to be able to Restart
+// AAU I want to be able to go to Next Question
+// AAU I want to be able to Exit the game.
+
+// MVP Goals
+// To have a UI of the game
+// To Display the Score
+// To Display the name of the Player
+// To Ask a questions and give multiple choice answers
+
+// Stretch Goals
+// To have a Restart, Exit, Next button
+// To add animation/sounds to the UI
+// To give hints
+// To showcase progress (Ex. if you're on question 7 and there are 10 questions total - it will display 7/10 on the UI)
+
+
+// psuedocode
+// Step 1: Load Start Page: Click to Play. Enter Player Name. Display playername/score on each window.
+// Step 1B: Rules Page *Stretch Goal*
+//Step 2: Enter game, Display Question, Give the player the ability to pick an answer out of 4 options
+//step 3: if correct, showcase green and then can proceed to next question, if wrong, showcase red and proceed to next question.
+//step 3B: display -> Q#/totalQ# *Stretch Goal*
+//step 4: Once all the questions are answered, display score and state the quiz is over.
