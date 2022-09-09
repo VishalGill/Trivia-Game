b27e3cf835d1cb7397656afdf4a4bd0eda4e2df8
VishalGill <vgill96@ymail.com>
2022-09-09 15:59:25 -0400

    Redid Basic  HTML & CSS

---
+++ trivia-game/p1.js
@@ -0,0 +1,7 @@
+let ul = document.getElementById('ul');
+let nextButton = document.getElementById('btnNext');
+let quizbox = document.getElementById('questionBox');
+let answer1 = document.getElementById('answer1');
+let answer2 = document.getElementById('answer2');
+let answer3 = document.getElementById('answer3');
+let answer4 = document.getElementById('answer4');
